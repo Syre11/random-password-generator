7ade6bfb51cc00c6970fee0a828bda0bb8427767
“Spencer <srye1140@gmail.com>
2022-10-08 14:18:33 -0700

    Adds JS code for password generator

--- script.js
+++ script.js
@@ -14,8 +14,79 @@ generateBtn.addEventListener("click", writePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
 
 function generatePassword() {
-///////////////////////// WRITE YOUR CODE HERE /////////////////////////
-
+    var passLength = prompt("How long would you like the password to be?");
+    passLength = Number(passLength)
+    if (passLength < 8) {
+      alert("Password is too short, must be between 8 and 128 characters.")
+      return;
+    } else if (passLength > 128) {
+      alert("Password is too long, must be between 8 and 128 characters.");
+      return;
+    }  else {
+      alert("Password is a valid length.")
+    }
+    var lowercase = confirm ("Would you like to include lowercase characters?")
+    if (lowercase) {
+      alert("Lowercase characters will be included.")
+    } else {
+      alert("Lowercase characters will NOT be included.")
+    }
+    var uppercase = confirm ("Would you like to include uppercase characters?")
+    if (uppercase) {
+      alert("Uppercase characters will be included.")
+    } else {
+      alert("Uppercase characters will NOT be included.")
+    }
+    var numeric = confirm ("Would you like to include numeric characters?")
+    if (numeric) {
+      alert("Numeric characters will be included.")
+    } else {
+      alert("Numeric characters will NOT be included.")
+    }
+    var special = confirm ("Would you like to include special characters?")
+    if (special) {
+      alert("Special characters will be included.")
+    } else {
+      alert("Special characters will NOT be included.")
+    }
+  
+    var chars = ""
+  
+    if (lowercase) {
+      chars += "abcdefghijklmnopqrstuvwxyz"
+    } 
+    if (uppercase) {
+      chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    if (numeric) {
+      chars += "0123456789"
+    }  
+    if (special) {
+      chars += " !#$%&'()*+,-./:;<=>?@[]^_`{|}~\u0022\u005c" 
+    }  
+    if (chars === "") {
+      alert("At least one character type must be selected. Please try again.")
+      return;
+    }
+  
+    // console.log(passLength)
+    // console.log(lowercase)
+    // console.log(uppercase)
+    // console.log(numeric)
+    // console.log(special)
+    // console.log(chars)
+  
+    var password = "";
+    for (var i = 0, n = chars.length; i < passLength; i++) {
+      password += chars.charAt(Math.floor(Math.random() * n))
+    }
+  
+    console.log(password)
+  
+    //shows password in popup alert
+    // window.alert(password)
+  
+    document.getElementById("password").innerHTML = password
 }
 
 /*
